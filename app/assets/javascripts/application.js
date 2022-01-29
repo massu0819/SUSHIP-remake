dbaf69c51999434f10f1109b43ce386085a75a0f
massu0819 <massu4869@gmail.com>
2022-01-29 12:40:57 +0000

    [Add]ヘッダー修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,22 @@
 //= require_tree .
 /*global jQuery $*/
 
+$(window).on('load',function(){
 
+
+  $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+
+  //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+
+ //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
+  $('.splashbg').on('animationend', function() {
+    $('.visual-logo').addClass('appear');
+
+      //この中に動かしたいJSを記載
+  });
+  //=====ここまで背景が伸びた後に動かしたいJSをまとめる
+
+});
 
 $(function(){
   $('.btn-trigger').on('click', function() {
@@ -52,3 +67,4 @@ $(function($) {
    ('form').fadeOut(500);
    ('.wrapper').addClass('form-success');
 });
+
